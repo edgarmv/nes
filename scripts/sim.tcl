74fd29f31a622d38a4bfd6bf861ba3cb3c3f920e
Edgar Vedvik <edgar.vedvik@gmail.com>
2020-04-04 19:07:20 +0200

    scripts sim.tcl: Add syntax checking before simulation

--- scripts/sim.tcl
+++ scripts/sim.tcl
@@ -1,5 +1,20 @@
-start_gui
-
 open_project build/NES.xpr
+
+set errors [check_syntax -fileset sources_1 -return_string]
+
+if {$errors != ""} {
+    puts "$errors"
+    exit
+}
+
+set errors [check_syntax -fileset sim_1 -return_string]
+
+if {$errors != ""} {
+    puts "$errors"
+    exit
+}
+
+
+start_gui
 
 launch_simulation
